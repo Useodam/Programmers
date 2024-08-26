bbef9542d33202188513be3a14b0ad0b6acfc99e
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2024-08-27 00:15:22 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        while(order > 0){
+            int n = order%10;
+            order = order/10;
+            if(n%3 == 0 && n != 0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
