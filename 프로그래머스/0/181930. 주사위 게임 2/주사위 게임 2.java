71dc5f9df71e210fd4731be7b2e4c91cf2d09da9
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2024-09-12 22:18:42 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -6,7 +6,7 @@ class Solution {
             answer = a+b+c;
         } else if(a==b && b==c){
             answer = (a+b+c) * (a*a + b*b + c*c) * ((a*a*a) + (b*b*b) + (c*c*c));
-        } else if(a==b || a==c || b==c){
+        } else {
             answer = (a+b+c) * (a*a + b*b + c*c);
         }
         
