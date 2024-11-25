e7f4af6d53540596cd52b0e4d43b3408bad68587
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2024-11-25 23:35:02 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        
+        for(int i=0; i<a.length; i++){
+            int sum = 0;
+            sum = a[i] * b[i];
+            answer += sum;    
+        }
+        
+        return answer;
+    }
+}
